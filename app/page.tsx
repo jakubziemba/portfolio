767013360f233d79e0fd82040b17e5390743f058
Jakub Ziemba <ziemba.jak@gmail.com>
2024-08-28 19:33:44 +0200

    (fix) desktop padding

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SocialList from "./_components/social-list";
 
 export default function Home() {
   return (
-    <section className="mx-auto max-w-screen-sm px-4 lg:px-0">
+    <section className="mx-auto max-w-screen-sm px-4 lg:px-0 lg:pt-10">
       <AboutSection />
       <RecentWork />
       <SocialList />
